6ff4c6e543d82083594af838b1cc01571bf3df43
Abemelekermi <103841321+Abemelekermi@users.noreply.github.com>
2024-09-03 21:54:47 +0300

    fixed some bugs

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\ABEME\Desktop\telegram-bot\.venv
+set VIRTUAL_ENV=c:\Users\ABEME\Desktop\Telegram_cli\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
